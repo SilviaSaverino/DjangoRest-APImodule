0d5760d31094eeb614d2657468a02a39ba02b438
SilviaSaverino <>
2023-04-20 14:10:48 +0000

    add urls path for profile

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 
+
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()

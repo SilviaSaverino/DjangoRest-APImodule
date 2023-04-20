98d8ed6c2fe41fe5bae4d487b38284679a25833e
SilviaSaverino <>
2023-04-20 14:46:00 +0000

    create serializers for profile model

---
+++ profiles/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Profile
+
+#BELOW:
+# We’ll create a ProfileSerializer class and inherit  
+# from ModelSerializer. We’ll specify ‘owner’ as a  
+# ReadOnlyField so that it can’t be edited. 
+# We’ll  also populate it with the owner's username.
+# In the Meta class, we’ll point to our Profile  model 
+# and specify the fields we’d like to  
+# include in the response. 
+# be explicit about which  fields you are including, 
+# because you might want  
+# to add another field to the Profile model later  on, 
+# that you don’t want to be included in the serializer.
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializer.ReadOnlyField(source='owner_username')
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image'
+        ]

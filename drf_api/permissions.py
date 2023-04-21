69068387ca5bef8c98b54bfdf1e926ae6d788da0
SilviaSaverino <>
2023-04-21 12:25:15 +0000

    add permission classes in ProfileDetail and update its get_object method

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -1,14 +1,15 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permission.BasePermission):
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
-    def has_object_permission(self,request,view,obj):
+    def has_object_permission(self, request, view, obj):
         """
         Check if the user has permission to access the object.
 
         """
-        if request.method in permission.SAFE-METHOD:
+        if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user

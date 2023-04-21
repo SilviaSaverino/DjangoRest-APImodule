ec978a8b3c275db2bdf977166a16a619a619e21c
SilviaSaverino <>
2023-04-21 13:11:06 +0000

    add serializer for Post model

---
+++ posts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from posts.models import Post
+
+class PostSerializer(serializer.ModelSerializer):
+    owner = serializer.ReadOnlyField(source='owner_username')
+    is_owner = serializer.SerializerMethodField()
+    profile_id = serializer.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializer.ReadOnlyField(source='owner.profile.img.url')
+
+    def get_is_owner(PostSerializer):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model: Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title', 'content', 'image'
+        ]

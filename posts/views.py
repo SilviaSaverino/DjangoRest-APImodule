a7dff14a126329a851863b7c2a42c2b70d0dc1bf
SilviaSaverino <>
2023-04-27 16:29:13 +0000

    refactor posts and profiles views file with generics

--- posts/views.py
+++ posts/views.py
@@ -1,85 +1,116 @@
-from rest_framework import status, permissions
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
-
-class PostList(APIView):
-
-    serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = PostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
 
 
-class PostDetail(APIView):
+class PostList(generics.ListCreateAPIView):
     """
-    A view that returns the details of a single post.
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
 
-    def get_object(self, pk):
-        """
-        Retrieve a single post instance by primary key
-        or return a 404 error.
-        """
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        """
-        Retrieve and serialize a single post instance by id.
-        """
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, context={'request': request}
-        )
-        return Response(serializer.data)
 
-    def put(self, request, pk):
-        """
-        Update a single post instance by primary key.
-        """
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data,  context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        """
-        Delete a single post instance by primary key.
-        """
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+#--------------------------------------REFACTORED CODE ABOVE VS ORIGINAL BELOW:
+# from rest_framework import status, permissions
+# from django.http import Http404
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .models import Post
+# from .serializers import PostSerializer
+# from drf_api.permissions import IsOwnerOrReadOnly
+
+# class PostList(APIView):
+
+#     serializer_class = PostSerializer
+#     permission_classes = [
+#         permissions.IsAuthenticatedOrReadOnly
+#     ]
+
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(
+#             posts, many=True, context={'request': request}
+#         )
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = PostSerializer(
+#             data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(
+#                 serializer.data, status=status.HTTP_201_CREATED
+#             )
+#         return Response(
+#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+#             )
+
+
+# class PostDetail(APIView):
+#     """
+#     A view that returns the details of a single post.
+#     """
+#     serializer_class = PostSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get_object(self, pk):
+#         """
+#         Retrieve a single post instance by primary key
+#         or return a 404 error.
+#         """
+#         try:
+#             post = Post.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, post)
+#             return post
+#         except Post.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk):
+#         """
+#         Retrieve and serialize a single post instance by id.
+#         """
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(
+#             post, context={'request': request}
+#         )
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         """
+#         Update a single post instance by primary key.
+#         """
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data,  context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         """
+#         Delete a single post instance by primary key.
+#         """
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response(
+#             status=status.HTTP_204_NO_CONTENT
+#         )
